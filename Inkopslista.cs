6246a32b0c060b76ee6f6b08b7ab9196de9c6547
Aldo651A <aldito43ariasg@hotmail.se>
2026-09-12 17:03:24 +0200

    Har lagt till  int taotal = 0  och total +=  produktpriser samt ytterligare en Console.Writline med total.

--- Inkopslista.cs
+++ Inkopslista.cs
@@ -8,7 +8,11 @@ produktPriser.Add(32);
 produktNamn.Add("Ost");
 produktPriser.Add(89);
 
+int total = 0;
+
 for (int i = 0; i < produktNamn.Count; i++)
 {
     Console.WriteLine($" {i + 1}  {produktNamn[i]} - {produktPriser[i]} kr");
+    total += produktPriser[i];
 }
+Console.WriteLine($"{total} kr");
