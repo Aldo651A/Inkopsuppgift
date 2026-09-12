1c4c00384cd5dcba9807e44fef27574fff1c2a74
Aldo651A <aldito43ariasg@hotmail.se>
2026-09-12 16:22:56 +0200

    Har lag till övriga produkter och priser.  Har börjat på forloopen .

--- Inkopslista.cs
+++ Inkopslista.cs
@@ -1,5 +1,14 @@
 List<string>produktNamn = [];
 List<int>produktPriser = [];
 
-produktNamn.Add("Mjölk");produktPriser.Add(15);
+produktNamn.Add("Mjölk");
+produktPriser.Add(15);
+produktNamn.Add("Bröd");
+produktPriser.Add(32);
+produktNamn.Add("Ost");
+produktPriser.Add(89);
 
+for (int i = 0; i < produktNamn.Count; i++)
+{
+    Console.WriteLine($"{i + 1}");
+}
