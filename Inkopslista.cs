89cfb0909b943905f4a092e362f26839ca9bf28e
Aldo651A <aldito43ariasg@hotmail.se>
2026-09-12 23:30:31 +0200

    har tagit bort meny och avänder istället  for loop och if satser  som ligger inuti varandra.. Detta bnlir nog rätt.

--- Inkopslista.cs
+++ Inkopslista.cs
@@ -1,35 +1,61 @@
 List<string>produktNamn = [];
-List<int>produktPriser = [];
+List<int>produktPriserna = [];
 
 produktNamn.Add("Mjölk");
-produktPriser.Add(15);
+produktPriserna.Add(15);
 produktNamn.Add("Bröd");
-produktPriser.Add(32);
+produktPriserna.Add(32);
 produktNamn.Add("Ost");
-produktPriser.Add(89);
+produktPriserna.Add(89);
 
 while (true)
 {
-    // Meny
     
-
     int total = 0;
 
- or (int i = 0; i < produktNamn.Count; i++)
+    for (int i = 0; i < produktNamn.Count; i++)
     {
-        Console.WriteLine($" {i + 1}  {produktNamn[i]} - {produktPriser[i]} kr");
-        total += produktPriser[i];
+        Console.WriteLine($" {i + 1}  {produktNamn[i]} - {produktPriserna[i]} kr");
+        total += produktPriserna[i];
             
     }
     Console.WriteLine($"    Total {total} kr");
 
+   
+   
     Console.WriteLine("Skriv en vara, ett nummer eller Avsluta: ");
     string input = Console.ReadLine();
 
-    if (Val == "produktNamn ")
+
+    if (input == Avsluta)
     {
-        Console.Write("Ange varans namn: ");
+        break;
+    }
+
+    if (int.TryParse(input, out int position))
+    {
+
+        if (position >= 1 && position <= produktNamn.Count)
+        {
+            produktNamn.RemoveAt(position -1);
+            produktPriserna.RemoveAt(position-1);
+            Console.WriteLine("Varan har tagits bort ");
+        }  
+        else
+        {
+
+            Console.WriteLine("Nummeret finns inte i listan");
+        }
+ 
+    
     }
 
 
+
+
+
+
+
 }
+
+
